ef62a9e1dbfc73eb6f18eb508c591409326ebca6
Jaagarn <Jaagarn@YORUICHI>
2022-09-27 15:49:11 +0200

    Improvments made during tech interview

--- stunning-enigma/Assets/Scripts/Tile.cs
+++ stunning-enigma/Assets/Scripts/Tile.cs
@@ -13,4 +13,21 @@ public class Tile : MonoBehaviour
 
     [SerializeField]
     public bool canGoMinusX;
+
+    public bool CanGoDirection(Vector3 direction)
+    {
+        if (Vector3.right.Equals(direction))
+            return canGoPlusX;
+
+        if (Vector3.left.Equals(direction))
+            return canGoMinusX;
+
+        if (Vector3.forward.Equals(direction))
+            return canGoPlusZ;
+
+        if (Vector3.back.Equals(direction))
+            return canGoMinusZ;
+
+        return false;
+    }
 }
